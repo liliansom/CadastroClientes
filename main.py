d4e4ae9ddd7b95b16d57635b3d00f157c5e5aa36
Lilian Matsumoto <109561962+liliansom@users.noreply.github.com>
2023-02-08 23:32:16 -0300

    Abertura do programa

---
+++ main.py
@@ -0,0 +1,14 @@
+from Projeto import Criar
+from tkinter import Tk
+
+
+def main():
+    raiz = Tk()
+    raiz.title("LM Cadastro de Clientes")
+    frame = Criar(raiz)
+    raiz.mainloop()
+
+
+if __name__ == '__main__':
+    main()
+    
