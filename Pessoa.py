6d9632dace11ea5d155630a538c9e709eeffcf30
Lilian Matsumoto <109561962+liliansom@users.noreply.github.com>
2023-02-11 02:00:03 -0300

    Progresso diário

--- Pessoa.py
+++ Pessoa.py
@@ -3,21 +3,9 @@ import pyodbc
 
 # BACK-END
 class Pessoa:
-    def __init__(self, nome,
-                 data,
-                 cpf,
-                 cep,
-                 rua,
-                 num,
-                 bairro,
-                 comp,
-                 cidade,
-                 estado,
-                 pais,
-                 tel1,
-                 tel2,
-                 email,
-                 obs):
+    def __init__(self, cad, nome, data, cpf, cep, rua, num, bairro, comp,
+                 cidade, estado, pais, tel1, tel2, email, obs):
+        self.cad = cad
         self.nome = nome
         self.data = data
         self.cpf = cpf
@@ -33,7 +21,7 @@ class Pessoa:
         self.tel2 = tel2
         self.email = email
         self.obs = obs
-
+        self.busca = []
 
     def adicionar(self):
         dadosconnect = "Driver={SQL Server};Server=.;Database=BDCadastro;"
@@ -55,22 +43,43 @@ class Pessoa:
                             '{self.tel1}', 
                             '{self.tel2}',
                             '{self.email}',
-                            '{self.obs}') ''')
+                            '{self.obs}'); ''')
         cursor.commit()
         cursor.close()
         connection.close()
 
     def excluir(self):
-    if len(EntId.get()) < 0:
-        messagebox.showerror(
-            "Erro 1",
-            'Adicione o Id do jogo...')
-        return
+        dadosconnect = "Driver={SQL Server};Server=.;Database=BDCadastro;"
+        connection = pyodbc.connect(dadosconnect)
+        cursor = connection.cursor()
+        # INSERIR MESSAGEBOX PARA CONFIRMAR AÇÃO
+        cursor.execute(f''' DELETE FROM 
+                                Cadastro2
+                            WHERE 
+                                NumCad='{self.cad}'; ''')
+        cursor.commit()
+        cursor.close()
+        connection.close()
 
-    cursor.execute(f'''
-    DELETE FROM 
-        Catalogo 
-    WHERE 
-        Id='{EntId.get()}' ''')
-    cursor.commit()
+    def procurar(self):
+        dadosconnect = "Driver={SQL Server};Server=.;Database=BDCadastro;"
+        connection = pyodbc.connect(dadosconnect)
+        cursor = connection.cursor()
+        cursor.execute(f'''
+                    SELECT 
+                        NumCad, Nome, DataNascimento, CPF, CEP, Logradouro, 
+                        Número, Bairro, Complemento, Cidade, Estado, 
+                        País, Telefone1, Telefone2, Email, Observação
+                    FROM 
+                        Cadastro2 
+                    WHERE
+                        NumCad=? OR
+                        Nome=?
+                    ''', (self.cad, self.nome))
+
+        self.busca = cursor.fetchone()
+        cursor.close()
+        connection.close()
+        return self.busca
+
 
