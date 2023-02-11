192523afc75952ccd1ec40f006d0f3f1176ec496
Lilian Matsumoto <109561962+liliansom@users.noreply.github.com>
2023-02-11 14:36:21 -0300

    Update Pessoa.py

--- Pessoa.py
+++ Pessoa.py
@@ -24,8 +24,6 @@ class Pessoa:
         self.busca = []
 
     def adicionar(self):
-        dadosconnect = "Driver={SQL Server};Server=.;Database=BDCadastro;"
-        connection = pyodbc.connect(dadosconnect)
         cursor = connection.cursor()
         cursor.execute(f''' INSERT INTO Cadastro2 (Nome, DataNascimento, CPF, CEP, Logradouro, 
         Número, Bairro, Complemento, Cidade, Estado, País, Telefone1, Telefone2, Email, Observação)
@@ -45,12 +43,8 @@ class Pessoa:
                             '{self.email}',
                             '{self.obs}'); ''')
         cursor.commit()
-        cursor.close()
-        connection.close()
 
     def excluir(self):
-        dadosconnect = "Driver={SQL Server};Server=.;Database=BDCadastro;"
-        connection = pyodbc.connect(dadosconnect)
         cursor = connection.cursor()
         # INSERIR MESSAGEBOX PARA CONFIRMAR AÇÃO
         cursor.execute(f''' DELETE FROM 
@@ -58,28 +52,37 @@ class Pessoa:
                             WHERE 
                                 NumCad='{self.cad}'; ''')
         cursor.commit()
-        cursor.close()
-        connection.close()
 
     def procurar(self):
-        dadosconnect = "Driver={SQL Server};Server=.;Database=BDCadastro;"
-        connection = pyodbc.connect(dadosconnect)
         cursor = connection.cursor()
         cursor.execute(f'''
                     SELECT 
-                        NumCad, Nome, DataNascimento, CPF, CEP, Logradouro, 
-                        Número, Bairro, Complemento, Cidade, Estado, 
-                        País, Telefone1, Telefone2, Email, Observação
+                        *
                     FROM 
                         Cadastro2 
                     WHERE
-                        NumCad=? OR
-                        Nome=?
-                    ''', (self.cad, self.nome))
+                        NumCad='{self.cad}' OR Nome='{self.nome}' 
+''')
 
-        self.busca = cursor.fetchone()
-        cursor.close()
-        connection.close()
-        return self.busca
+        result = cursor.fetchone()
+        self.cad = str(result[0])
+        self.nome = str(result[1])
+        self.data = str(result[2])
+        self.cpf = str(result[3])
+        self.cep = str(result[4])
+        self.rua = str(result[5])
+        self.num = str(result[6])
+        self.bairro = str(result[7])
+        self.comp = str(result[8])
+        self.cid = str(result[9])
+        self.estado = str(result[10])
+        self.pais = str(result[11])
+        self.tel1 = str(result[12])
+        self.tel2 = str(result[13])
+        self.email = str(result[14])
+        self.obs = str(result[15])
+        print(result)
+        return self
+
 
 
