cc7b2735d75660b1d8ff1b0c47e8beb7559c5eb0
Lilian Matsumoto <109561962+liliansom@users.noreply.github.com>
2023-02-10 23:07:31 -0300

    Progresso diário
    Realizado defs: __init__, adicionar e excluir em progresso.

--- Pessoa.py
+++ Pessoa.py
@@ -1,29 +1,76 @@
+import pyodbc
+
+
 # BACK-END
 class Pessoa:
-    def __init__(self, nome):
-        self.nome = self.entnome
+    def __init__(self, nome,
+                 data,
+                 cpf,
+                 cep,
+                 rua,
+                 num,
+                 bairro,
+                 comp,
+                 cidade,
+                 estado,
+                 pais,
+                 tel1,
+                 tel2,
+                 email,
+                 obs):
+        self.nome = nome
+        self.data = data
+        self.cpf = cpf
+        self.cep = cep
+        self.rua = rua
+        self.num = num
+        self.bairro = bairro
+        self.comp = comp
+        self.cid = cidade
+        self.estado = estado
+        self.pais = pais
+        self.tel1 = tel1
+        self.tel2 = tel2
+        self.email = email
+        self.obs = obs
 
 
-    def adicionar():
-        dadosconnect = "Driver={SQL Server};Server=DESKTOP-AB1DE95;Database=BDCadastro;"
+    def adicionar(self):
+        dadosconnect = "Driver={SQL Server};Server=.;Database=BDCadastro;"
         connection = pyodbc.connect(dadosconnect)
         cursor = connection.cursor()
-        cursor.execute(f'''
-          INSERT INTO 
-              Clientes (
-              Nome)
-          VALUES(
-              '{self.nome.get().strip().upper()}' ''')
+        cursor.execute(f''' INSERT INTO Cadastro2 (Nome, DataNascimento, CPF, CEP, Logradouro, 
+        Número, Bairro, Complemento, Cidade, Estado, País, Telefone1, Telefone2, Email, Observação)
+                            VALUES('{self.nome}', 
+                            '{self.data}', 
+                            '{self.cpf}',
+                            '{self.cep}',
+                            '{self.rua}', 
+                            '{self.num}', 
+                            '{self.bairro}', 
+                            '{self.comp}', 
+                            '{self.cid}', 
+                            '{self.estado}', 
+                            '{self.pais}', 
+                            '{self.tel1}', 
+                            '{self.tel2}',
+                            '{self.email}',
+                            '{self.obs}') ''')
         cursor.commit()
-        connection.close()
         cursor.close()
-
-
-'''    def procurar(self):
-
-    def modificar(self):
+        connection.close()
 
     def excluir(self):
+    if len(EntId.get()) < 0:
+        messagebox.showerror(
+            "Erro 1",
+            'Adicione o Id do jogo...')
+        return
 
-    def limpar(self):
-'''
+    cursor.execute(f'''
+    DELETE FROM 
+        Catalogo 
+    WHERE 
+        Id='{EntId.get()}' ''')
+    cursor.commit()
+
