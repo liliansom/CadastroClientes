c4c2de8a771dea9ec0e8ecea7aea5278572a65e4
Lilian Matsumoto <liliansmatsumoto@gmail.com>
2023-02-15 02:20:34 -0300

    Adicionada função Buscar CEP
    Adicionada funcionalidade buscar cep, irá preencher os dados de rua, bairro, cidade e estado via API

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 # This is a sample Python script.
-from Projeto import *
+from Criar import *
 from tkinter import Tk
 
 
 def main():
-    raiz = Tk()
+    raiz = Tk()    
     raiz.title("LM Cadastro de Clientes")
     frame = Criar(raiz)
     raiz.mainloop()
