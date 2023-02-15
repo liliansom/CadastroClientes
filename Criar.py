3aaa3a37636677d5e00a54299982964e21e7f933
Lilian Matsumoto <109561962+liliansom@users.noreply.github.com>
2023-02-15 02:26:52 -0300

    Implementada função Buscar CEP
    Adicionado botão para busca do CEP, irá retornar rua, bairro, cidade e estado

--- Criar.py
+++ Criar.py
@@ -3,10 +3,10 @@ import tkinter.ttk
 from tkinter import *
 from tkinter import PhotoImage
 from Pessoa import Pessoa
+from Requisitar import Requisitar
+
 
 """Criação do layout do programa"""
-
-
 class Criar:
     def __init__(self, telaprincipal):
         self.telaprincipal = telaprincipal
@@ -19,19 +19,19 @@ class Criar:
         self.frame = tkinter.Frame(bg='#D4FFF7')
         self.frame.place(
             x=0,
-            y=136,
+            y=170,
             width=165,
             height=1000)
         self.botoes()
         self.entradas()
         self.labels()
 
+    # Criação dos botões
     def botoes(self):
-        # Criação dos botões
         self.botao = tkinter.Button()
         self.botao.place(
             x=20,
-            y=210,
+            y=244,
             height=40,
             width=120)
         self.botao.configure(text='Adicionar',
@@ -40,7 +40,7 @@ class Criar:
         self.botao1 = tkinter.Button()
         self.botao1.place(
             x=20,
-            y=270,
+            y=304,
             height=40,
             width=120)
         self.botao1.configure(text='Editar', font='Jost', command=self.cliceditar)
@@ -48,7 +48,7 @@ class Criar:
         self.botao2 = tkinter.Button()
         self.botao2.place(
             x=20,
-            y=330,
+            y=364,
             height=40,
             width=120)
         self.botao2.configure(text='Limpar', font='Jost', command=self.cliclimpar)
@@ -56,7 +56,7 @@ class Criar:
         self.botao3 = tkinter.Button()
         self.botao3.place(
             x=20,
-            y=390,
+            y=424,
             height=40,
             width=120)
         self.botao3.configure(text='Procurar', font='Jost', command=self.clicprocurar)
@@ -64,7 +64,7 @@ class Criar:
         self.botao4 = tkinter.Button()
         self.botao4.place(
             x=20,
-            y=450,
+            y=484,
             height=40,
             width=120)
         self.botao4.configure(text='Excluir', font='Jost', command=self.clicdeletar)
@@ -72,201 +72,211 @@ class Criar:
         self.botao5 = tkinter.Button()
         self.botao5.place(
             x=20,
-            y=520,
+            y=544,
             height=40,
             width=120)
         self.botao5.configure(text='Sair', font='Jost', command=self.telaprincipal.destroy)
 
-    # Criação das entradas de dados
+        self.botao6 = tkinter.Button()
+        self.botao6.place(
+            x=177,
+            y=310,
+            height=30,
+            width=40)
+        self.botao6.configure(text='CEP', font='Jost', command=self.cliccep)
 
+    # Criação das entradas de dados
     def entradas(self):
         self.entcad = tkinter.Entry(self.telaprincipal, relief="solid")
         self.entcad.place(
-            x=200,
-            y=175,
+            x=230,
+            y=209,
             width=75,
             height=30)
         self.entnome = tkinter.Entry(self.telaprincipal, relief="solid")
         self.entnome.place(
-            x=300,
-            y=175,
+            x=330,
+            y=209,
             width=300,
             height=30)
         self.entdata = tkinter.Entry(self.telaprincipal, relief="solid")
         self.entdata.place(
-            x=620,
-            y=175,
+            x=650,
+            y=209,
             width=175,
             height=30)
 
         self.entcpf = tkinter.Entry(relief="solid")
         self.entcpf.place(
-            x=820,
-            y=175,
+            x=850,
+            y=209,
             width=250,
             height=30)
         self.entcep = tkinter.Entry(relief="solid")
         self.entcep.place(
-            x=200,
-            y=275,
+            x=230,
+            y=309,
             width=100,
             height=30)
         self.entrua = tkinter.Entry(relief="solid")
         self.entrua.place(
-            x=350,
-            y=275,
+            x=380,
+            y=309,
             width=300,
             height=30)
         self.entnum = tkinter.Entry(relief="solid")
         self.entnum.place(
-            x=700,
-            y=275,
+            x=730,
+            y=309,
             width=100,
             height=30)
         self.entbairro = tkinter.Entry(relief="solid")
         self.entbairro.place(
-            x=850,
-            y=275,
+            x=880,
+            y=309,
             width=220,
             height=30)
         self.entcomplem = tkinter.Entry(relief="solid")
         self.entcomplem.place(
-            x=200,
-            y=350,
+            x=230,
+            y=384,
             width=250,
             height=30)
         self.entcid = tkinter.Entry(relief="solid")
         self.entcid.place(
-            x=500,
-            y=350,
+            x=530,
+            y=384,
             width=150,
             height=30)
         self.entest = tkinter.Entry(relief="solid")
         self.entest.place(
-            x=700,
-            y=350,
+            x=730,
+            y=384,
             width=150,
             height=30)
         self.entpais = tkinter.Entry(relief="solid")
         self.entpais.place(
-            x=900,
-            y=350,
+            x=930,
+            y=384,
             width=170,
             height=30)
         self.enttel1 = tkinter.Entry(relief="solid")
         self.enttel1.place(
-            x=200,
-            y=450,
+            x=230,
+            y=484,
             width=150,
             height=30)
         self.enttel2 = tkinter.Entry(relief="solid")
         self.enttel2.place(
-            x=425,
-            y=450,
+            x=455,
+            y=484,
             width=150,
             height=30)
         self.entemail = tkinter.Entry(relief="solid")
         self.entemail.place(
-            x=650,
-            y=450,
+            x=680,
+            y=484,
             width=200,
             height=30)
         self.entobs = tkinter.Text(relief="solid")
         self.entobs.place(
-            x=200,
-            y=525,
+            x=230,
+            y=559,
             width=400,
-            height=100)
+            height=80)
         self.entretorna = tkinter.Text(relief="solid")
         self.entretorna.place(
-            x=770,
-            y=525,
+            x=800,
+            y=559,
             width=300,
-            height=100)
+            height=80)
         return self
 
-    def labels(self):  # Criação das etiquetas
+    # Métodos auxiliares
+    # Criação das etiquetas
+    def labels(self):  
         self.labelcad = tkinter.Label(text="Cadastro", background='white')
         self.labelcad.place(
-            x=198,
-            y=150)
+            x=230,
+            y=184)
         self.labelmenu = tkinter.Label(text="Menu", background='#D4FFF7', font='Jost')
         self.labelmenu.place(
             x=55,
-            y=160)
+            y=194)
         self.labelnome = tkinter.Label(text="Nome", background='white')
         self.labelnome.place(
-            x=300,
-            y=150)
+            x=330,
+            y=184)
         self.labeldn = tkinter.Label(text="Data de Nascimento", background='white')
         self.labeldn.place(
-            x=620,
-            y=150)
+            x=650,
+            y=184)
         self.labelcpf = tkinter.Label(text="CPF", background='white')
         self.labelcpf.place(
-            x=820,
-            y=150)
+            x=850,
+            y=184)
         self.labelend = tkinter.Label(text="Endereço Residencial", background='white')
         self.labelend.place(
-            x=198,
-            y=225)
+            x=230,
+            y=259)
         self.labelcep = tkinter.Label(text="CEP", background='white')
         self.labelcep.place(
-            x=198,
-            y=250)
+            x=230,
+            y=285)
         self.labelrua = tkinter.Label(text="Logradouro", background='white')
         self.labelrua.place(
-            x=350,
-            y=250)
+            x=380,
+            y=285)
         self.labelnum = tkinter.Label(text="Número", background='white')
         self.labelnum.place(
-            x=700,
-            y=250)
+            x=730,
+            y=285)
         self.labelbairro = tkinter.Label(text="Bairro", background='white')
         self.labelbairro.place(
-            x=850,
-            y=250)
+            x=880,
+            y=285)
         self.labelcomplem = tkinter.Label(text="Complemento", background='white')
         self.labelcomplem.place(
-            x=198,
-            y=325)
+            x=230,
+            y=359)
         self.labelcid = tkinter.Label(text="Cidade", background='white')
         self.labelcid.place(
-            x=500,
-            y=325)
+            x=530,
+            y=359)
         self.labelest = tkinter.Label(text="Estado", background='white')
         self.labelest.place(
-            x=700,
-            y=325)
+            x=730,
+            y=359)
         self.labelest = tkinter.Label(text="País", background='white')
         self.labelest.place(
-            x=900,
-            y=325)
+            x=930,
+            y=359)
         self.labelest = tkinter.Label(text="Contatos", background='white')
         self.labelest.place(
-            x=200,
-            y=400)
+            x=230,
+            y=434)
         self.labeltel1 = tkinter.Label(text="Telefone 1", background='white')
         self.labeltel1.place(
-            x=200,
-            y=425)
+            x=230,
+            y=459)
         self.labeltel2 = tkinter.Label(text="Telefone 1", background='white')
         self.labeltel2.place(
-            x=425,
-            y=425)
+            x=455,
+            y=459)
         self.labelemail = tkinter.Label(text="E-mail", background='white')
         self.labelemail.place(
-            x=650,
-            y=425)
+            x=680,
+            y=459)
         self.labelobs = tkinter.Label(text="Observações", background='white')
         self.labelobs.place(
-            x=200,
-            y=500)
+            x=230,
+            y=534)
         self.labelobs = tkinter.Label(text="Busca", background='white')
         self.labelobs.place(
-            x=770,
-            y=500)
+            x=800,
+            y=534)
 
+    #Método para transformar entrada em string
     def trataent(self):
         self.cad = str(self.entcad.get())
         self.nome = str(self.entnome.get())
@@ -332,7 +342,6 @@ class Criar:
         self.entretorna.delete('1.0', 'end')
 
     # Método para retornar valores para o usuário
-
     def inserir(self, pessoa):
         self.entcad.delete('0', 'end')
         self.entnome.delete('0', 'end')
@@ -353,6 +362,8 @@ class Criar:
         self.entemail.insert(0, '{}'.format(pessoa.email))
         self.entobs.insert('1.0', '{}'.format(pessoa.obs))
 
+    # Métodos com funções de cada botão 
+    # Método para função do botão Adicionar
     def clicadc(self):
         dadosstr = self.trataent()
         self.pessoa = Pessoa(*dadosstr)
@@ -375,7 +386,7 @@ class Criar:
         self.pessoa.procurar()
         self.inserir(self.pessoa)
 
-
+    # Método para função do botão editar
     def cliceditar(self):
         dados = self.buscaent()
         self.pessoa = Pessoa(*dados)
@@ -384,4 +395,15 @@ class Criar:
         self.pessoanova = Pessoa(*dadosnovos)
         self.pessoanova.editar()
         print(f'{self.pessoanova}')
+        
+    def cliccep(self):
+        dados = self.buscaent()
+        pessoa = dados[4]
+        self.residencia = Requisitar(pessoa)
+        self.residencia.request()
+        self.entrua.insert(0, '{}'.format(self.residencia.rua))
+        self.entbairro.insert(0, '{}'.format(self.residencia.bairro))
+        self.entcid.insert(0, '{}'.format(self.residencia.cidade))
+        self.entest.insert(0, '{}'.format(self.residencia.estado))
+        return
         
